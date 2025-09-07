9c426a8bfac2513e3c7c3a6cbc0054d2876dedaf
Hudson Amorim <hudson.eng.software@gmail.com>
2025-09-07 15:31:14 -0300

    Commit dia 07-09-2025

--- src/game/entities/Character.java
+++ src/game/entities/Character.java
@@ -1,4 +1,50 @@
 package game.entities;
 
-public class Character {
+public abstract class Character {
+    protected String name;
+    protected int health, attack, defense;
+
+    public Character(String name, int health, int attack, int defense) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
+    public abstract void attack(Character target);
+
+    public boolean isAlive(){
+        return health > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
